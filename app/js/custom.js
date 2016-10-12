dddb3e8f73f832f85be8a7b82e31ce2fb473734f
Максим <zhil.byl.max@gmail.com>
2016-10-12 18:36:14 +0600

    Version 0.1

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,11 @@
-console.log('hi2')
+jQuery(document).ready(function(){
+    $objWindow = $(window);
+    $('div[data-type="parallax"]').each(function(){
+        var $bgObj = $(this);
+        $(window).scroll(function() {
+            var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+            var coords = '100% '+ yPos + 'px';
+            $bgObj.css({ backgroundPosition: coords });
+        });
+    });
+});
