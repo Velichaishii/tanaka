8154bdf823412d2909a974d503a3a84f593009ae
Максим <zhil.byl.max@gmail.com>
2016-10-24 18:24:10 +0600

    Version 0.12.

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,63 @@
 $(document).ready(function(){
+
+
+
+    var doc = $(document),
+        siteWrap = $('.site-wrap');
+    var siteWrapOverlay = $('.site-wrap-overlay');
+    var burgerMenuWrap = $('.burger-menu-wrap');
+
+
+    doc.click(function (e) {
+        var target = $(e.target),
+            popup = target.closest(".popup");
+        popup_open = target.closest(".popup-open");
+
+        if ((!popup.length > 0 ) && (!popup_open.length > 0 )) {
+            $(".popup").removeClass("visible");
+            siteWrap.removeClass("active");
+            siteWrap.width('100%');
+            siteWrapOverlay.fadeOut(300);
+        }
+
+    });
+
+
+
+    $(".btn-burger-menu").click(function(){
+        if(burgerMenuWrap.hasClass('visible') === true) {
+            $(".popup").removeClass("visible");
+            siteWrap.removeClass("active");
+            siteWrap.width('100%');
+            siteWrapOverlay.fadeOut(400);
+        } else {
+            burgerMenuWrap.addClass("visible");
+            siteWrap.addClass("active");
+            siteWrap.width(siteWrap.outerWidth());
+            siteWrapOverlay.fadeIn(300);
+        }
+
+    });
+
+    $(".btn-close").click(function(){
+        $(".popup").removeClass("visible");
+        siteWrap.removeClass("active");
+        siteWrap.width('100%');
+        siteWrapOverlay.fadeOut(400);
+    });
+
+
+
+    $(".menu__item-first-level").mousemove(function(){
+        $(this).find(".menu__list-second-level").stop().fadeIn(200);
+    });
+
+    $(".menu__item-first-level").mouseout(function(){
+        $(this).find(".menu__list-second-level").stop().fadeOut(200);
+    });
+
+
+
     $('[data-type="bg_paralax"]').each(function(){
         var $bgobj = $(this);
         var pos_this = $(this).offset().top;
@@ -12,4 +71,7 @@ $(document).ready(function(){
 
         });
     });
+
+
+
 });
